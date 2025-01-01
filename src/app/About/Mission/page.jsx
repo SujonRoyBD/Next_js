2fb5b9f9ae5a33db9b2f220100fa157426e79938
SujonRoyBD <sujonroy537527@gmail.com>
2025-01-01 13:06:28 +0600

    first commit

---
+++ src/app/About/Mission/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AboutLayout from '../Layout'
+
+const Mission = () => {
+  return (
+   <main>
+    <AboutLayout/>
+     <div>
+      dhaka
+    </div>
+   </main>
+  )
+}
+
+export default Mission
---
+++ src/app/About/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AboutLayout from './Layout'
+
+const About = () => {
+  return (
+   <main>
+    <AboutLayout/>
+     <div>
+      sujon roy
+    </div>
+   </main>
+  )
+}
+
+export default About
